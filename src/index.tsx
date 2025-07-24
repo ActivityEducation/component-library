00377e6034892edaee36ebc35e7a3003bf245ade
Brandon Bleau <bmbleau@gmail.com>
2025-07-24 00:44:55 -0400

    chore: update to release all components

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,27 @@
 // It exports all public components and the theme provider,
 // making them available for consumption by other applications.
 
+export * from './components/Badge';
 export * from './components/Button';
+export * from './components/Card';
+export * from './components/Checkbox';
+export * from './components/Flipper';
+export * from './components/Flyover';
+export * from './components/Icon';
+export * from './components/Input';
+export * from './components/List';
+export * from './components/Menu';
+export * from './components/ProgressBar';
+export * from './components/Radio';
+export * from './components/Select';
+export * from './components/Sidenav';
+export * from './components/Slider';
+export * from './components/Spinner';
+export * from './components/Tab';
+export * from './components/Table';
+export * from './components/Toolbar';
+export * from './components/VideoPlayer';
+
 // Export the theme object itself, useful for direct access to design tokens
 export * from './theme';
 // Export Emotion's ThemeProvider, which is essential for consuming applications
